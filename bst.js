34fb355c5e67292db8747465b00788db5e50d355
Pere <pere.solaclaver@gmail.com>
2020-10-14 22:37:59 +0100

    constructor BST

---
+++ bst.js
@@ -0,0 +1,10 @@
+class BinarySearchTree {
+	constructor(value) {
+        // value of root node
+        this.value = value;
+        // value of left node
+        this.left = null;
+        // value of right node
+		this.right = null;
+	}
+}

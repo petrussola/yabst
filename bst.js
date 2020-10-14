dafb76ede7170e9037c0ff81bc47f8e243b72111
Pere <pere.solaclaver@gmail.com>
2020-10-14 23:52:34 +0100

    Add insert method and test it

--- bst.js
+++ bst.js
@@ -14,6 +14,37 @@ class BinarySearchTree {
 		this.right = null;
 		return this;
 	}
+
+	// insert value
+	insert(value) {
+		// if root node is empty, set root node value to value
+		if (value === this.value) {
+			this.value = value;
+			// if value < root value
+		} else if (value < this.value) {
+			// if this.left is null
+			if (this.left === null) {
+				// set this.left to new bst passing the value
+				this.left = new BinarySearchTree(value);
+
+				// otherwise, call insert on this.left
+			} else {
+				this.left.insert(value);
+			}
+			// if value > root value
+		} else if (value > this.value) {
+			// if this.right is null
+			if (this.right === null) {
+				// set this.right to new bst passing the value
+				this.right = new BinarySearchTree(value);
+				// otherwise, call insert on this.right
+			} else {
+				this.right.insert(value);
+			}
+		}
+		// return bst
+		return this;
+	}
 }
 
 module.exports = BinarySearchTree;
--- bst.test.js
+++ bst.test.js
@@ -4,10 +4,30 @@ let bst = new BinarySearchTree(10);
 
 afterEach(() => {
 	bst.cleanup();
+	bst.insert(5);
+	bst.insert(7);
+	bst.insert(3);
+	bst.insert(8);
+	bst.insert(2);
+	bst.insert(15);
+	bst.insert(12);
+	bst.insert(17);
+	bst.insert(16);
 });
 
 test('Binary Search Tree gets created', () => {
 	expect(bst.value).toEqual(10);
 	expect(bst.left).toEqual(null);
 	expect(bst.right).toEqual(null);
+});
+
+test('Insert values', () => {
+	expect(bst.left.value).toEqual(5);
+	expect(bst.left.right.right.value).toEqual(8);
+	expect(bst.left.left.left.value).toEqual(2);
+	expect(bst.left.left.left.right).toEqual(null);
+	expect(bst.right.value).toEqual(15);
+	expect(bst.right.right.left.value).toEqual(16);
+	expect(bst.right.left.value).toEqual(12);
+	expect(bst.right.left.right).toEqual(null);
 });
